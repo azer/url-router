51f4892adb48238d013a4694dfb450f1211ed60f
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-16 04:19:01 -0800

    #Add

--- routes.go
+++ routes.go
@@ -15,10 +15,14 @@ func New (patterns...string) *Router {
 	router := &Router{make(Routes)}
 
 	for _, pattern := range patterns {
-		router.Context[pattern] = NewRoute(pattern)
+		router.Add(pattern)
 	}
 
 	return router
+}
+
+func (router *Router) Add (pattern string) {
+	router.Context[pattern] = NewRoute(pattern)
 }
 
 func (router *Router) Match (url string) *Match {
